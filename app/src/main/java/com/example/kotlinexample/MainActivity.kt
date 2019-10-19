6eeb7bf4166563c54a09c180d5d8f94df1505df3
hkd0694 <gksrudehd123@naver.com>
2019-10-19 17:57:45 +0900

    fix : Code Comment

--- app/src/main/java/com/example/kotlinexample/MainActivity.kt
+++ app/src/main/java/com/example/kotlinexample/MainActivity.kt
@@ -7,10 +7,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //Extensions (확장)
+        //확장 함수는 어떤 클래에 밖에서, 마치 그 클래스의 멤버인 것처럼 선언된 메서드를 말한다.
+        //클래스 내부에 있지 않지만 그 클래스 내부 멤버에 접근할 수 있고, 클래스 멤버 메서드와 동일한 방식으로 호출할 수 있다.
         recyclerview.adapter = MainAdapter()
         recyclerview.layoutManager = LinearLayoutManager(this)
 
--- app/src/main/java/com/example/kotlinexample/MainAdapter.kt
+++ app/src/main/java/com/example/kotlinexample/MainAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_main.view.*
 
+//만약 생성자를 만들 때 매개변수가 필요할 경우, 주 생성자를 만들어 주자
 class MainAdapter: RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
 
     var items: MutableList<MainData> = mutableListOf(MainData("Title1","Content1"),
@@ -13,6 +14,9 @@ class MainAdapter: RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = MainViewHolder(parent)
 
     override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        //let 함수 item
+        //함수를 호출하는 객체를 이어지는 블록의 인자로 넘기고, 해당 블록의 결과 값을 반환하는 코틀린의 함수이다.
+        //이 let 함수가 안전한 호출 연산자와 함께 사용되면 Nullable 타입을 사용할 수 없는 곳에 Nullable 타입의 변수를 사용할 수 있는 방법이 된다.
         items[position].let {
             item -> with(holder){
                 tvTitle.text = item.title
@@ -21,9 +25,11 @@ class MainAdapter: RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
         }
     }
 
-    override fun getItemCount(): Int  = items.size
+    //!! 연산자는 절대 null 값이 올수 없는 상황에서
+    // nullable 타입의 변수를 사용해야 할 때 Null Check 가 없으면 컴퍼일할 수 없는 번거로운 일을 막기 위해 만들어 졌다.
+    override fun getItemCount(): Int  = items!!.size
 
-    class MainViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+    inner class MainViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
         LayoutInflater.from(parent.context).inflate(R.layout.item_main,parent,false)){
         val tvTitle = itemView.tv_main_title
         val tvContent = itemView.tv_main_content
